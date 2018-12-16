e0c2748fc1c2b197d4921e95336fcc38825d23c3
ivanaivanovicc <iivancica90@gmail.com>
2018-12-16 01:55:59 +0100

    Random platforme i pokreti

--- 1.c
+++ 1.c
@@ -1,19 +1,39 @@
 #include<GL/glut.h>
 #include<stdlib.h>
-
+#include<math.h>
+#include<stdio.h>
+#include<time.h>
 #define TIMER_ID 1
 #define TIMER_INTERVAL 8
-
+#define MAX 1000
 static void display(void);
 static void on_keyboard(unsigned char key, int x, int y);
+static void key(int k, int x, int y);
 static void on_reshape(int width, int height);
 static void on_timer(int id);
 static void init_lights();
+static void on_timer(int id);
 
+float animation_parameter, a=0;
+int animation_ongoing, animation_ongoing2=0;
+void platforma(float x);
+float p=0;
+float x_curr=0, y_curr=0;
+int n=0;
+int xKoordinate[] = {-6, -3, 0, 3, 6};
+float x1=0;
+int k=0;
 
-float animation_parameter;
-int animation_ongoing;
+int ukupno=0;
+float zL=5.9;
 
+typedef struct {
+    float xP, yP, zP;
+    float lP, dP, nP;
+}Platforma;
+
+void postavi_platformu();
+Platforma nizP[MAX];
 
 int main(int argc, char** argv){
 
@@ -28,15 +48,17 @@ int main(int argc, char** argv){
 	glutDisplayFunc(display);
 	glutReshapeFunc(on_reshape);
 	glutKeyboardFunc(on_keyboard);
+    	glutSpecialFunc(key);
 	glClearColor( 0.737255, 0.560784, 0.560784, 0);
 	
 	glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+    	glEnable(GL_CULL_FACE);
+    	glCullFace(GL_BACK);
 
+	
 	animation_parameter = -1.5;
 	animation_ongoing = 0;
-	
+	animation_ongoing=0;
 	glutMainLoop();
 	return 0;
 }
@@ -62,8 +84,8 @@ static void on_timer(int id)
     if (TIMER_ID != id)
         return;
 
-    animation_parameter += 0.08;
-
+    animation_parameter += 0.2;
+    k++;
     glutPostRedisplay();
 
     if (animation_ongoing) {
@@ -71,6 +93,42 @@ static void on_timer(int id)
     }
 }
 
+static void on_timer2(int id)
+{
+    if (TIMER_ID != id)
+        return;
+
+    a += 0.13;
+
+    glutPostRedisplay();
+
+    if (animation_ongoing2) {
+        glutTimerFunc(TIMER_INTERVAL, on_timer2, TIMER_ID);
+    }
+}
+
+
+
+static void key(int k, int x, int y)
+{
+	
+	switch(k){
+		case GLUT_KEY_LEFT:
+			x1-=0.15;
+			glutPostRedisplay();
+			break;
+		case GLUT_KEY_RIGHT:
+			x1+=0.15;
+			glutPostRedisplay();
+			break;
+		case GLUT_KEY_UP:
+			if (!animation_ongoing2) {
+            			animation_ongoing2 = 1;
+            			glutTimerFunc(TIMER_INTERVAL, on_timer2, TIMER_ID);
+            		}
+			break;
+	}
+}
 static void on_keyboard(unsigned char key, int x, int y)
 {
     switch (key) {
@@ -103,15 +161,14 @@ static void on_reshape(int width, int height)
 
 static void init_lights()
 {
-    GLfloat light_ambient[] = { 0, 0, 0, 1 };
-    GLfloat light_diffuse[] = { 1, 1, 1, 1 };
-    GLfloat light_specular[] = { 1, 1, 1, 1 };
-    GLfloat model_ambient[] = { 0.4, 0.4, 0.4, 1 };
-	
+	GLfloat light_ambient[] = { 0, 0, 0, 1 };
+	GLfloat light_diffuse[] = { 1, 1, 1, 1 };
+	GLfloat light_specular[] = { 1, 1, 1, 1 };
+	GLfloat model_ambient[] = { 0.4, 0.4, 0.4, 1 };
 	
 	glEnable(GL_DEPTH_TEST);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
@@ -124,7 +181,7 @@ static void init_lights()
 
 static void material2()
 {
-    GLfloat no_material[] = { 1.0, 1.0, 1.0, 1 };
+	GLfloat no_material[] = { 1, 1.0, 1.0, 1 };
 	GLfloat material_ambient[] = { 0.8, 0.9, 0.5, 1 };
 	GLfloat material_diffuse[] = { 0.2, 0.6, 0.3, 1 };
 	GLfloat material_specular[] = { 0.1, 0.1, 0.1, 1 };
@@ -137,8 +194,24 @@ static void material2()
 	glMaterialf(GL_FRONT, GL_SHININESS, shine);
 	glMaterialfv(GL_FRONT, GL_EMISSION, no_material);
 }	
+void postavi_platformu(){
 
-
+	int br = rand()%3+2;
+	
+	int i;
+	for(i=0; i<br; i++){
+		Platforma p;
+		p.xP = xKoordinate[rand()%5];
+		p.yP = 0;
+		p.zP = -k-5;
+		p.lP = p.xP - 1;
+		p.dP = p.xP + 1;
+		p.nP = p.zP;
+		nizP[ukupno]=p;
+		ukupno++;
+	}
+		
+}
 
 static void display(void){
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -148,35 +221,45 @@ static void display(void){
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(0, 10, 30,
-		  	  0, 0, 0,
+		  0, 0, 0,
 	    	  0, 1, 0);
 	
 	init_lights();
+	if(k%10==0)
+		postavi_platformu();
+		
 	
-		material2();
-		glColor3f(0, 1, 1);
-    	glBegin(GL_POLYGON);
-        	glVertex3f(3, -1, -3);
-        	glVertex3f(-3, -1, -3);
-        	glVertex3f(-3, -1, 3);
-        	glVertex3f(3, -1, 3);
-    	glEnd();
-    
-    
-    material2();
-		glColor3f(0, 1, 1);
-    	glBegin(GL_POLYGON);
-        	glVertex3f(10, -1, -10);
-        	glVertex3f(4, -1, -10);
-        	glVertex3f(4, -1, -4);
-        	glVertex3f(10, -1, -4);
-    	glEnd();
-    
+    	int j;
+	for(j=0; j<ukupno; j++)
+	{
+		glPushMatrix();
+			glTranslatef(0+nizP[j].xP, 0, nizP[j].zP+animation_parameter);
+			material();
+			material2();
+			glColor3f(0, 1, 1);
+    		glBegin(GL_POLYGON);
+        		glVertex3f(nizP[j].xP, -5, -nizP[j].xP);
+        		glVertex3f(nizP[j].xP-6, -5, -nizP[j].xP);
+        		glVertex3f(nizP[j].xP-6, -5, -(nizP[j].xP-6));
+        		glVertex3f(nizP[j].xP, -5, -(nizP[j].xP-6));
+    		glEnd();	
+  		glPopMatrix();	
+  		
+	}
 	glPushMatrix();
-		glRotatef(animation_parameter * 80, 1, 0, 0);
+        	glTranslatef(0+x1, 0, 6);
+       		if(a<5)
+			glTranslatef(0, a, 0);
+		if(a>=5 && 10-a>0)
+			glTranslatef(0, 5+5-a, 0);
+		else if(a>=5 && animation_ongoing2==1){
+			a=0;
+			animation_ongoing2=0;
+		}
+       		glRotatef(animation_parameter*10, 1, 0, 0);
 		material();
 		glutSolidSphere(1, 22, 22);
-    glPopMatrix();	
+    	glPopMatrix();	
 	
 	glutSwapBuffers();
 
